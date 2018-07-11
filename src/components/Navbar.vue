e5a0a68010665120d05b3e34f0ebb5b4f067a2da
Daniel Freire <danjfreire@gmail.com>
2018-07-10 23:47:14 -0300

    starting Vue

---
+++ src/components/Navbar.vue
@@ -0,0 +1,51 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-fixed-top navbar-dark bg-dark">
+    <div class="container">
+        <img class="navbar-brand mb-0" src="../assets/DotaProLogo.png">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSite">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSite">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link text-silver" href="#">Tournaments</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-silver" href="#">Teams</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-silver" href="#">Players</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
